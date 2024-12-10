fed297c15d08b5962fad1dea349c33dbdc9ff9e8
HyeokE <jhjeong00@gmail.com>
2024-12-10 12:50:14 +0900

    fix: icon size 28 to 24

--- src/assets/HomeIcon.tsx
+++ src/assets/HomeIcon.tsx
@@ -30,8 +30,8 @@ const HomeIcon = () => {
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        width="28"
-        height="28"
+        width="24"
+        height="24"
         viewBox="0 0 24 24"
         fill="none"
         stroke="currentColor"
