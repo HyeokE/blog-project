f79139605cb37c0b3fd609f00486ef9dd245c0d9
HyeokE <jhjeong00@gmail.com>
2024-12-14 15:55:43 +0900

    fix: remove cmdk

--- src/components/Command.tsx
+++ src/components/Command.tsx
@@ -1,39 +1,39 @@
-'use client';
-import { Command } from 'cmdk';
-import { useEffect, useState } from 'react';
-
-const CommandMenu = () => {
-  const [open, setOpen] = useState(false);
-
-  // Toggle the menu when ⌘K is pressed
-  useEffect(() => {
-    const down = (e: KeyboardEvent) => {
-      if (e.key === 'k' && (e.metaKey || e.ctrlKey)) {
-        e.preventDefault();
-        setOpen((open) => !open);
-      }
-    };
-
-    document.addEventListener('keydown', down);
-    return () => document.removeEventListener('keydown', down);
-  }, []);
-
-  return (
-    <Command>
-      <Command.Input />
-      <Command.List>
-        <Command.Empty>No results found.</Command.Empty>
-        <Command.Group heading="Letters">
-          <Command.Item>a</Command.Item>
-          <Command.Item>b</Command.Item>
-          <Command.Separator />
-          <Command.Item>c</Command.Item>
-        </Command.Group>
-
-        <Command.Item>Apple</Command.Item>
-      </Command.List>
-    </Command>
-  );
-};
-
-export default CommandMenu;
+// 'use client';
+// import { Command } from 'cmdk';
+// import { useEffect, useState } from 'react';
+//
+// const CommandMenu = () => {
+//   const [open, setOpen] = useState(false);
+//
+//   // Toggle the menu when ⌘K is pressed
+//   useEffect(() => {
+//     const down = (e: KeyboardEvent) => {
+//       if (e.key === 'k' && (e.metaKey || e.ctrlKey)) {
+//         e.preventDefault();
+//         setOpen((open) => !open);
+//       }
+//     };
+//
+//     document.addEventListener('keydown', down);
+//     return () => document.removeEventListener('keydown', down);
+//   }, []);
+//
+//   return (
+//     <Command>
+//       <Command.Input />
+//       <Command.List>
+//         <Command.Empty>No results found.</Command.Empty>
+//         <Command.Group heading="Letters">
+//           <Command.Item>a</Command.Item>
+//           <Command.Item>b</Command.Item>
+//           <Command.Separator />
+//           <Command.Item>c</Command.Item>
+//         </Command.Group>
+//
+//         <Command.Item>Apple</Command.Item>
+//       </Command.List>
+//     </Command>
+//   );
+// };
+//
+// export default CommandMenu;
--- src/container/home/components/PostSearch.tsx
+++ src/container/home/components/PostSearch.tsx
@@ -1,13 +1,12 @@
 'use client';
 import React from 'react';
 import { SearchIcon } from '@/assets/SearchIcon';
-import CommandMenu from '@/components/Command';
 
 const PostSearch = () => {
   return (
     <>
       <SearchIcon onClick={() => console.log('search')} />
-      <CommandMenu />
+      {/*<CommandMenu />*/}
     </>
   );
 };
