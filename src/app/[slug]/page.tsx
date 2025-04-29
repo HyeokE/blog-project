6149b2a082cd5701de332f8229ded8095a8c1497
HyeokE <jhjeong00@gmail.com>
2025-04-29 21:00:50 +0900

    feat: Add dynamic post detail page and upgrade Next.js to 15.3.1

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,45 @@
+import { getAllPosts } from '@/apis/NotionService';
+
+import type { Metadata } from 'next';
+import type { NotionPosts } from '@/models/NotionPosts';
+
+import { getPageDetail } from '@/utils/notion/getPageDetail';
+import PostDetailPage from '@/container/PostDetail/PostDetailPage';
+
+export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params;
+  const post = await getPageDetail(slug);
+
+  return <PostDetailPage post={post} />;
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}): Promise<Metadata> {
+  const { slug: id } = await params;
+  const properties = await getPageDetail(id);
+
+  return {
+    title: properties.title || 'Default Title',
+    openGraph: {
+      images: [`/api/og?title=${encodeURIComponent(properties.title)}`], // 동적 이미지 경로
+    },
+    metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'), // 기본 URL 설정
+  };
+}
+export const dynamic = 'force-static';
+export const revalidate = 60;
+
+export async function generateStaticParams() {
+  const posts: NotionPosts = await getAllPosts({
+    includePages: false,
+  });
+
+  return posts
+    .filter((post) => post.id) // id가 있는 post만 포함
+    .map((post) => ({
+      slug: String(post.id),
+    }));
+}
